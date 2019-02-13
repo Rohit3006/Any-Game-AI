d7fe9963de7502c341a5f69603a654ab8b8798bf
Rohit Neppalli <neppalli.rohit@gmail.com>
2019-02-12 18:53:25 -0600

    Set up the conv net

--- Conv_Net.py
+++ Conv_Net.py
@@ -1,50 +1,58 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import numpy as np
 import cv2
 import os
 import pickle
 import pandas as pd
 import glob
+from keras.utils import to_categorical
 
 def replace(df, column, x):
     unique = list(set(df[column].values))
     return unique.index(x)
 
-def create_conv_net(output_neurons=1, layers=1, epochs=10, verbose=2, size=20):
-    file_dir = "/Users/rohitneppalli/Library/Mobile Documents/com~apple~CloudDocs/Documents/workspace/Python/Python/Flappy Bird/Frames/"
+
+def create_conv_net(output_neurons=1, layers=1, epochs=10, verbose=2, size_x=20, size_y=20, train_size=0.7):
+    file_dir = "Frames/"
     x = []
+
+    np.random.seed(1)
 
     files = sorted(os.listdir(file_dir))
 
     for file in files:
         file_name = os.path.join(file_dir, file)
         array = cv2.imread(file_name)
-        array = cv2.cvtColor(array, cv2.COLOR_BGR2RGB)
-        array = cv2.resize(array, (size, size))
+        array = cv2.resize(array, (size_y, size_x))
         x.append(array)
 
     x = np.array(x) / 255
 
-    df = pd.read_csv(
-        "/Users/rohitneppalli/Library/Mobile Documents/com~apple~CloudDocs/Documents/workspace/Python/Python/Flappy Bird/inputs.csv")
+    df = pd.read_csv("inputs.csv")
+    y_set = set(np.array(df["Input"]))
     y = np.array(df["Input"].apply(lambda x: replace(df, "Input", x)))
+    y = to_categorical(y, len(set(y)))
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9)
-    x_train = x_train.reshape(-1, size**2)
-    x_test = x_test.reshape(-1, size**2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size)
+
     model = Sequential()
 
-    model.add(Dense(size**2))
+    model.add(Conv2D(64, (3,3)))
+    model.add(MaxPool2D((2,2)))
     for i in range(layers):
-        model.add(Dense(50, activation='relu'))
-    model.add(Dense(100, activation='relu'))
+        model.add(Conv2D(32, (2, 2)))
+        model.add(MaxPool2D((2, 2)))
+    model.add(Dropout(0.4))
+    model.add(Flatten())
+    model.add(Dense(50, activation='relu'))
     model.add(Dense(output_neurons, activation='softmax'))
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=epochs, verbose=2,
-              validation_data=(x_test, y_test))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=epochs, verbose=verbose, validation_data=(x_test, y_test))
 
-    return model
+    model.save("model.model")
+
+    return list(y_set), model
