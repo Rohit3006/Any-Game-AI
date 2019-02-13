140f0bf62136c209ad1a9e5da878c1ea935fa2f1
Rohit Neppalli <neppalli.rohit@gmail.com>
2019-02-12 19:02:56 -0600

    Can change between models

--- Play Game.py
+++ Play Game.py
@@ -15,18 +15,21 @@ import pandas as pd
 import glob
 from Deep_Neural_Net import create_deep_neural_net
 from Conv_Net import create_conv_net
-import pyscreenshot
+
+MODEL_TYPE = "conv" # MODEL_TYPE should be either deep or conv
 
 df = pd.read_csv("inputs.csv")
-inputs = list(set(df.Input.values))  # list(set(df.Input.values))
+inputs = list(set(df.Input.values))
 
 size_x = 70
 size_y = 30
-if os.path.exists("model.model"):
+if os.path.exists("model2.model"):
     model = load_model("model.model")
 else:
-    inputs, model = create_deep_neural_net(output_neurons=len(
-        inputs), layers=4, epochs=20, size_x=size_x, size_y=size_y, train_size=0.80)
+    if MODEL_TYPE == "deep":
+        inputs, model = create_deep_neural_net(output_neurons=len(inputs), layers=4, epochs=20, size_x=size_x, size_y=size_y, train_size=0.80)
+    elif MODEL_TYPE == "conv":
+        inputs, model = create_conv_net(output_neurons=len(inputs), layers=2, epochs=20, size_x=size_x, size_y=size_y,train_size=0.8)
 
 for i in range(5, 0, -1):
     print(i)
@@ -40,7 +43,7 @@ while True:
         print("escape")
         break
 
-    if keyboard.is_pressed('p'):  # break loop
+    if keyboard.is_pressed('p'):  # pause
         if not paused:
             print("paused")
             paused = True
@@ -51,21 +54,24 @@ while True:
         continue
 
     frame_number += 1
-    if frame_number % 200 != 0:  # Get every 400th frame
+    if frame_number % 200 != 0:  # Get every 200th frame
         continue
 
     frame = np.array(ImageGrab.grab(bbox=(360, 220, 1080, 520)))
-    #frame = np.array(pyscreenshot.grab()[479:960, 73:714])
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.Canny(frame, 50, 200)
-    frame = preprocessing.scale(cv2.resize(frame, (size_y, size_x)))
-    frame = frame.reshape(-1, size_x*size_y)
+
+    if MODEL_TYPE == "conv":
+        frame = cv2.resize(frame, (size_y, size_x))
+        frame = frame / 255
+    elif MODEL_TYPE == "deep":
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.Canny(frame, 50, 200)
+        frame = preprocessing.scale(cv2.resize(frame, (size_y, size_x)))
+        frame = frame.reshape(-1, size_x*size_y)
 
     prediction = model.predict(frame)
     print(prediction)
 
     prediction = np.argmax(prediction)
-
     print(inputs[prediction])
 
     if inputs[prediction] != "blank":
