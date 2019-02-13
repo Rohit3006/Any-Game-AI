9822dc484d965314f7f50792685b6398ab1af116
Rohit Neppalli <neppalli.rohit@gmail.com>
2019-02-12 18:24:34 -0600

    Changed data collection and added dropouts

--- Collect_Data.py
+++ Collect_Data.py
@@ -6,51 +6,58 @@ import csv
 from PIL import ImageGrab
 import os
 import keyboard
+import pyscreenshot
+import random
 
 frame_number = -1
-number = 1
 
-possible_inputs = "abcdefghijklmnopqrstuvwxyz".split() + ["ctrl"]
+possible_inputs = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ") + ["space", "ctrl", "shift"]
 
 directory = "Frames/"
 length = len([name for name in os.listdir(directory) if os.path.isfile(name)])
-if length > 0:
-    number = length # Continue progress
+number = length + 1
 
-file = open("inputs.csv", "w")
+file = open("inputs.csv", "a")
 writer = csv.writer(file)
-writer.writerow(["Frame Number", "Input"])
+
+if number == 1:
+	writer.writerow(["Frame Number", "Input"])
 
 for i in range(5, 0, -1):
-    print(i)
-    time.sleep(1)
+	print(i)
+	time.sleep(1)
 
 max_number = 5000
 max_string = str(max_number)
 while number < max_number:
-    frame_number += 1
-    if frame_number % 100 != 0: # Get every 100th frame
-        continue
+	frame_number += 1
+	if frame_number % 200 != 0: # Get every 200th frame
+		continue
 
-    frame = np.array(ImageGrab.grab(bbox=(450, 200, 950, 750))) # Set bounds of frame to whatever for the chosen game
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	frame = np.array(ImageGrab.grab(bbox=(360, 220, 1080, 520))) # Set bounds of frame to whatever for the chosen game
+	#frame = np.array(pyscreenshot.grab()[479:960, 73:714])
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    string = str(number)
+	string = str(number)
 
-    if keyboard.is_pressed('escape'):
-        break
+	if keyboard.is_pressed('escape'):
+		break
 
-    input_list = []
-    for poss in possible_inputs:
-        if keyboard.is_pressed(poss):
-            input_list.append(poss)
+	input_list = []
+	for poss in possible_inputs:
+		if keyboard.is_pressed(poss):
+			input_list.append(poss)
 
-    if len(input_list) == 0:
-        writer.writerow(["{0}".format(number), "blank"])
-    else:
-        input_string = "+".join(input_list)
-        writer.writerow(["{0}".format(number), input_string])
+	if len(input_list) == 0:
+		rand_num = random.randint(0, 10) # randomly determine if blank frame saved or not
+		if rand_num < 3:
+			writer.writerow(["{0}".format(number), "blank"])
+		else:
+			continue
+	else:
+		input_string = "+".join(input_list)
+		writer.writerow(["{0}".format(number), input_string])
 
-    cv2.imwrite(os.path.join(directory, "Frame {0}.png".format(
-        "0" * (len(max_string) - len(string)) + string)), frame)
-    number += 1
+	cv2.imwrite(os.path.join(directory, "Frame {0}.png".format(
+		"0" * (len(max_string) - len(string)) + string)), frame)
+	number += 1
--- Play Game.py
+++ Play Game.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import ImageGrab
 import keyboard
 import os
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -15,37 +15,51 @@ import pandas as pd
 import glob
 from Deep_Neural_Net import create_deep_neural_net
 from Conv_Net import create_conv_net
+import pyscreenshot
 
 df = pd.read_csv("inputs.csv")
 inputs = list(set(df.Input.values))  # list(set(df.Input.values))
 
-size = 50
+size_x = 70
+size_y = 30
 if os.path.exists("model.model"):
-    model = pickle.load(open("model.model", "rb"))
+    model = load_model("model.model")
 else:
     inputs, model = create_deep_neural_net(output_neurons=len(
-        inputs), layers=4, epochs=10, size=size, train_size=0.7)
+        inputs), layers=4, epochs=20, size_x=size_x, size_y=size_y, train_size=0.80)
 
 for i in range(5, 0, -1):
     print(i)
     time.sleep(1)
 
 frame_number = 0
+paused = False
 
 while True:
-    if keyboard.is_pressed('Q'):  # break loop
+    if keyboard.is_pressed('escape'):  # break loop
         print("escape")
         break
 
-    frame_number += 1
-    if frame_number % 100 != 0:  # Get every 100th frame
+    if keyboard.is_pressed('p'):  # break loop
+        if not paused:
+            print("paused")
+            paused = True
+        else:
+            paused = False
+
+    if paused:
         continue
 
-    frame = np.array(ImageGrab.grab(bbox=(450, 200, 950, 750)))
+    frame_number += 1
+    if frame_number % 200 != 0:  # Get every 400th frame
+        continue
+
+    frame = np.array(ImageGrab.grab(bbox=(360, 220, 1080, 520)))
+    #frame = np.array(pyscreenshot.grab()[479:960, 73:714])
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.Canny(frame, 0, 200)
-    frame = preprocessing.scale(cv2.resize(frame, (size, size)))
-    frame = frame.reshape(-1, size**2)
+    frame = cv2.Canny(frame, 50, 200)
+    frame = preprocessing.scale(cv2.resize(frame, (size_y, size_x)))
+    frame = frame.reshape(-1, size_x*size_y)
 
     prediction = model.predict(frame)
     print(prediction)
