c0529fe990432570a148797a4a8471b93a4ae8ac
Rohit3006 <neppalli.rohit@gmail.com>
2019-02-13 22:47:02 -0600

    Updated all

--- Deep_Neural_Net.py
+++ Deep_Neural_Net.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 import numpy as np
@@ -14,7 +14,7 @@ def replace(df, column, x):
     unique = list(set(df[column].values))
     return unique.index(x)
 
-def create_deep_neural_net(output_neurons=1, layers=1, epochs=10, verbose=2, size=20, train_size=0.7):
+def create_deep_neural_net(output_neurons=1, layers=1, epochs=10, verbose=2, size_x=20, size_y=20, train_size=0.7):
     file_dir = "Frames/"
     x = []
 
@@ -26,8 +26,8 @@ def create_deep_neural_net(output_neurons=1, layers=1, epochs=10, verbose=2, siz
         file_name = os.path.join(file_dir, file)
         array = cv2.imread(file_name)
         array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
-        array = cv2.Canny(array, 0, 200)
-        array = cv2.resize(array, (size, size))
+        array = cv2.Canny(array, 50, 200)
+        array = cv2.resize(array, (size_y, size_x))
         x.append(preprocessing.scale(array))
 
     x = np.array(x)
@@ -38,17 +38,21 @@ def create_deep_neural_net(output_neurons=1, layers=1, epochs=10, verbose=2, siz
     y = to_categorical(y, len(set(y)))
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=train_size)
-    x_train = x_train.reshape(-1,size**2)
-    x_test = x_test.reshape(-1,size**2)
+    x_train = x_train.reshape(-1,size_x*size_y)
+    x_test = x_test.reshape(-1, size_x*size_y)
     model = Sequential()
 
-    model.add(Dense(size**2, activation='relu'))
+    model.add(Dense(size_x*size_y, activation='relu'))
     for i in range(layers):
         model.add(Dense(100, activation='relu'))
+        model.add(Dropout(0.1))
     model.add(Dense(50, activation='relu'))
+    model.add(Dropout(0.3))
     model.add(Dense(output_neurons, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=epochs, verbose=2, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, epochs=epochs, verbose=verbose, validation_data=(x_test, y_test))
+
+    model.save("model.model")
 
     return list(y_set), model
--- Play Game.py
+++ Play Game.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import ImageGrab
 import keyboard
 import os
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -16,41 +16,62 @@ import glob
 from Deep_Neural_Net import create_deep_neural_net
 from Conv_Net import create_conv_net
 
-df = pd.read_csv("inputs.csv")
-inputs = list(set(df.Input.values)) #list(set(df.Input.values))
+MODEL_TYPE = "deep" # MODEL_TYPE should be either deep or conv
 
-size = 50
-if os.path.exists("model.model"):
-    model = pickle.load(open("model.model", "rb"))
+df = pd.read_csv("inputs.csv")
+inputs = list(set(df.Input.values))
+
+size_x = 70
+size_y = 30
+if os.path.exists("model2.model"):
+    model = load_model("model.model")
 else:
-    inputs, model = create_deep_neural_net(output_neurons=len(inputs), layers=4, epochs=10, size=size, train_size=0.7)
+    if MODEL_TYPE == "deep":
+        inputs, model = create_deep_neural_net(output_neurons=len(inputs), layers=4, epochs=20, size_x=size_x, size_y=size_y, train_size=0.80)
+    elif MODEL_TYPE == "conv":
+        inputs, model = create_conv_net(output_neurons=len(inputs), layers=2, epochs=20, size_x=size_x, size_y=size_y,train_size=0.8)
 
 for i in range(5, 0, -1):
     print(i)
     time.sleep(1)
 
 frame_number = 0
+paused = False
 
 while True:
-    if keyboard.is_pressed('Q'): # break loop
+    if keyboard.is_pressed('escape'):  # break loop
         print("escape")
         break
 
-    frame_number += 1
-    if frame_number % 100 != 0:  # Get every 100th frame
+    if keyboard.is_pressed('p'):  # pause
+        if not paused:
+            print("paused")
+            paused = True
+        else:
+            paused = False
+
+    if paused:
         continue
 
-    frame = np.array(ImageGrab.grab(bbox=(450, 200, 950, 750)))
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.Canny(frame, 0, 200)
-    frame = preprocessing.scale(cv2.resize(frame, (size, size)))
-    frame = frame.reshape(-1,size**2)
+    frame_number += 1
+    if frame_number % 200 != 0:  # Get every 200th frame
+        continue
+
+    frame = np.array(ImageGrab.grab(bbox=(360, 220, 1080, 520)))
+
+    if MODEL_TYPE == "conv":
+        frame = cv2.resize(frame, (size_y, size_x))
+        frame = frame / 255
+    elif MODEL_TYPE == "deep":
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = cv2.Canny(frame, 50, 200)
+        frame = preprocessing.scale(cv2.resize(frame, (size_y, size_x)))
+        frame = frame.reshape(-1, size_x*size_y)
 
     prediction = model.predict(frame)
     print(prediction)
 
     prediction = np.argmax(prediction)
-
     print(inputs[prediction])
 
     if inputs[prediction] != "blank":
